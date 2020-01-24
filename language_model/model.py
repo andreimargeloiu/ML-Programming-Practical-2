769b9e43df94ced753e424918d6f2fb1b1d02584
Andrei Margeloiu <andrei.margeloiu@gmail.com>
2020-01-24 10:48:42 +0000

    Added code

---
+++ language_model/model.py
@@ -0,0 +1,5 @@
+#TODO 1# Pick framework to use:
+
+#from model_tf1 import LanguageModelTF1 as LanguageModel
+#from model_tf2 import LanguageModelTF2 as LanguageModel
+#from model_torch import LanguageModelTorch as LanguageModel
